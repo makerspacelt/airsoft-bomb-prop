2b51a46c1f560441b1311704827e57ed060e270e
juodumas <juodumas@gmail.com>
2025-09-03 13:42:56 +0300

    esphome.sh: use docker or podman for building

--- sw/src-esphome/esphome.sh
+++ sw/src-esphome/esphome.sh
@@ -11,9 +11,21 @@ for d in /dev/ttyACM* /dev/ttyUSB*; do
     args+=("--volume=$d:$d")
 done
 
+if type docker >/dev/null 2>&1; then
+    container_cmd=docker
+    container_args=
+elif type podman >/dev/null 2>&1; then
+    container_cmd=podman
+    container_args="--userns keep-id"
+else
+    echo
+    echo "ERROR: docker or podman is required to run esphome"
+    echo
+    exit 1
+fi
+
 # shellcheck disable=SC2086
-exec podman run -it --rm \
-    --userns keep-id \
+exec $container_cmd run $container_args -it --rm \
     -e PLATFORMIO_SETTING_ENABLE_TELEMETRY=0 \
     -v "$PWD/..:/config" \
     "${args[@]}" \
